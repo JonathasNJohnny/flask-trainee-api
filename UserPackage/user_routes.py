356f831c854519a97813b3a3bda0d1bb61ec480b
JonathasNJohnny <jonathasnobrega59@gmail.com>
2024-05-05 20:29:15 -0300

    .

--- UserPackage/user_routes.py
+++ UserPackage/user_routes.py
@@ -8,36 +8,39 @@ def teste():
     print("Hello world")
     return "hello"
 
-@user_bp.route('/api/user/login', methods=['POST'])
+@user_bp.route('/api/user/login', methods=['GET'])
 def user_login():
     """
-    Login
-    ---
-    tags:
-      - Rotas de usuário
-    parameters:
-      - in: body
-        name: login_info
-        description: Informações de login (email e senha)
-        required: true
-        schema:
-          type: object
-          properties:
-            email:
-              type: string
-            senha:
-              type: string
-    responses:
-      200:
-        description: Logado
-    """
+    {}
+    """.format(swagger_login_description.strip())
     login_info = request.get_json()
     email = login_info.get("email")
     senha = login_info.get("senha")
     response = login_student(email, senha)
     return jsonify({"message": response})
 
-@user_bp.route('/testes',methods=['GET'])
-def teste():
-    print("Hello world")
-    return "Testante"
+
+
+#DESCRIÇÕES ---
+
+swagger_login_description = """
+Login
+---
+tags:
+  - Rotas de usuário
+parameters:
+  - in: body
+    name: login_info
+    description: Informações de login (email e senha)
+    required: true
+    schema:
+      type: object
+      properties:
+        email:
+          type: string
+        senha:
+          type: string
+responses:
+  200:
+    description: Logado
+"""
