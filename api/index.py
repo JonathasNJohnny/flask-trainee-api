99113acf3837d6adb61718eea9d568d498eb0cc1
Jonathas Nóbrega <57995489+JonathasNJohnny@users.noreply.github.com>
2024-05-05 15:18:46 -0300

    .

--- api/index.py
+++ api/index.py
@@ -1,6 +1,8 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flasgger import Swagger
 
 app = Flask(__name__)
+swagger = Swagger(app)
 
 @app.route('/')
 def home():
@@ -9,3 +11,16 @@ def home():
 @app.route('/about')
 def about():
     return 'About'
+
+@app.route('/api/example', methods=['GET'])
+def example_endpoint():
+    """
+    Exemplo de endpoint
+    ---
+    responses:
+      200:
+        description: Este é um exemplo de endpoint
+      404:
+        description: error
+    """
+    return jsonify({"message": "Este é um exemplo de endpoint"})
